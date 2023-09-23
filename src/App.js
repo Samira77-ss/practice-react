101d9f24c8813dd1fbf0e4e7e949566a256ec28d
Samira77-ss <samiraseyfii0709@gmail.com>
2023-09-23 10:01:07 +0330

    Added the GitHub url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <footer>
+        This project was coded by Samira Seyfi and is{" "}
+        <a href="https://github.com/Samira77-ss/practice-react" target="_blank">
+          open sourced on GitHub
+        </a>
+      </footer>
+
       <Weather city="Tokyo" />
     </div>
   );
