208d8284340d8c9745790a86a73b5f11acbd6bcc
Samira77-ss <samiraseyfii0709@gmail.com>
2023-09-24 13:56:16 +0330

    Added API integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather city="Tokyo" />
         <footer>
           This project was coded by Samira Seyfi and is{" "}
           <a
